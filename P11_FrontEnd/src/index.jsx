ef570286add749b9c18f6feddfb33bd7d8a2569f
Pauline Hesse <pipelettelive@gmail.com>
2024-12-23 10:36:12 +0100

    React 23/12/24

---
+++ P11_FrontEnd/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from "./reducers"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: true,
+});
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+);
+  
+reportWebVitals()

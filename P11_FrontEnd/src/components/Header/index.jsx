c0fa7fb59f961b174adfbd3637bc43d17fad1e01
Pauline Hesse <pipelettelive@gmail.com>
2025-01-21 10:32:43 +0100

    Modif header 20/01/2025

--- P11_FrontEnd/src/components/Header/index.jsx
+++ P11_FrontEnd/src/components/Header/index.jsx
@@ -7,13 +7,24 @@ function Header() {
 
   const token = localStorage.getItem("token");
 
-  if (localStorage !== null && token !== null) {
-    signOut.style.display = "none";
-  }
-  else {
-    signIn.style.display = "none";
-    signOut.style.display = "unset";
-  }
+  const currentUrl = window.location.pathname;
+
+  console.log(currentUrl, "current url")
+
+  // signOut.style.display = "none"
+
+  // if (currentUrl.includes('/User')) {
+
+  //   signOut.style.display = "unset"
+  // }
+
+  // if (localStorage !== null && token !== null) {
+  //   signOut.style.display = "none";
+  // }
+  // else {
+  //   signIn.style.display = "none";
+  //   signOut.style.display = "unset";
+  // }
   return (
     <nav className="main-nav">
       <Link to="/">
@@ -27,8 +38,8 @@ function Header() {
         </div>
       </Link>
       <Link to="/SignIn">
-        <div className="main-nav-item" id="signIn" style={""}><i className="fa fa-user-circle"></i>Sign In</div>
-        <div className="main-nav-item" id="signOut" style={""}><i className="fa fa-user-circle"></i>Sign Out</div>
+        <div className="main-nav-item" id="signIn" style={{display : "flex"}}><i className="fa fa-user-circle"></i>Sign In</div>
+        <div className="main-nav-item" id="signOut" style={{display : "none"}}><i className="fa fa-user-circle"></i>Sign Out</div>
       </Link>
     </nav>
   );

cc40744cf997daa46e05ba40dbb27224824b56e6
Pauline Hesse <pipelettelive@gmail.com>
2025-01-30 11:48:47 +0100

    Modif redux 30/01/2025

--- P11_FrontEnd/src/features/userSlice.js
+++ P11_FrontEnd/src/features/userSlice.js
@@ -25,24 +25,3 @@ export const userSlice = createSlice({
 export const { signIn, signOut } = userSlice.actions
 
 export default userSlice.reducer
-
-// export const userSlice = createSlice({
-//   name: "User",
-//   initialState: {
-//     user:null
-//   },
-//   reducers:{
-//     login: (state, action) => {
-//       state.user = action.payload;
-//     },
-//     logout: (state) => {
-//       state.user = null;
-//     },
-//   },
-// })
-
-// export const {login, logout} = userSlice.actions;
-
-// export const selectUser = (state) => state.user.user;
-
-// export default userSlice.reducer;
